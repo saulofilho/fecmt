2c6319bfa11b22c7ae9cb94c0785bd853d5fe885
Saulo Filho <32524964+saulofilho@users.noreply.github.com>
2020-09-10 11:18:44 -0300

    [fix] new pages: servicos e historia

---
+++ src/pages/Historia.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Historia = () => {
+  return (
+    <div className="Historia">
+      <h1>Historia.</h1>
+    </div>
+  )
+}
+
+export default Historia;
---
+++ src/pages/Servicos.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Servicos = () => {
+  return (
+    <div className="Servicos">
+      <h1>Servicos.</h1>
+    </div>
+  )
+}
+
+export default Servicos;
