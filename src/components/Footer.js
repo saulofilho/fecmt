fea2c681c366e87a73bca9a543768c74dac2ade8
saulo filho <saulosilvaf@gmail.com>
2022-01-08 22:00:08 -0300

    [feat] footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,20 +4,21 @@ import './Footer.scss'
 const FooterWrapper = () => {
   return (
     <footer>
-      <h4>
-        Federação dos Empregados no Comércio do Estado de Mato Grosso
-      </h4>
-      <br/>
-      <p>
-        Av. Historiador Rubens de Mendonça, n.º 2254
-        <br/>
-        Edifício American Business Center
-        <br/>
-        Bosque da Saúde, Cuiabá - MT
-        <br/>
-        CEP: 78050-000
-      </p>
-    </footer>
+      <div className="container">
+        <div className="footer-wrapper">
+          <div className="infos">
+            <h4>Federação dos Empregados</h4>
+            <h4>no Comércio do Estado de Mato Grosso</h4>
+          </div>
+          <div className="infos">
+            <p>Av. Historiador Rubens de Mendonça, n.º 2254</p>
+            <p>Edifício American Business Center</p>
+            <p>Bosque da Saúde, Cuiabá - MT</p>
+            <p>CEP: 78050-000</p>
+          </div>
+        </div>
+      </div>
+    </footer >
   )
 }
 
